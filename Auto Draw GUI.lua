277bc70a1146c918b79e2692f50c29ea5b34d7a3
Brudda <139983537+bruddaa@users.noreply.github.com>
2023-08-03 17:52:47 +0200

    Create Auto Draw GUI.lua

---
+++ Auto Draw GUI.lua
@@ -0,0 +1,219 @@
+-- MADE BY BRUDDA 
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+    Name = "Starving Artists auto draw",
+    LoadingTitle = "Find more on Discord",
+    LoadingSubtitle = "by Brudda",
+    ConfigurationSaving = {
+        Enabled = false,
+        FolderName = nil, -- Create a custom folder for your hub/game
+        FileName = "Big Hub"
+    },
+    Discord = {
+        Enabled = true,
+        Invite = "3NqYkMFH2C",
+        RememberJoins = false
+    }
+})
+
+local Tab = Window:CreateTab("Art")
+-- Anime Section
+local Section = Tab:CreateSection("IMPORTANT!! Wait at least 3-5 Minutes before submitting to avoid a Warning!")
+local Section = Tab:CreateSection("Anime")
+local Button = Tab:CreateButton({
+   Name = "Zoro (One Piece)",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/zoro%20one%20piece.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Monkey (One Piece)",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/Monkey%20(One%20Piece).lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Yumeko (Kakegurui)",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/Yumeko%20(Kakegurui).lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Ryota (Kakegurui)",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/Ryota%20(Kakegurui).lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Naruto",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/Naruto.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Goku",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/Goku.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Pikachu",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/Pikachu.lua"))();
+   end,
+})
+
+
+-- Streamer Section
+local Section = Tab:CreateSection("Streamer")
+local Button = Tab:CreateButton({
+   Name = "Speed with Cap",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/speed%20with%20cap.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Speed (Shake Pic)",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/speed%20shake.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Top G (with Car)",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/Top%20G.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Top G (Caught by the Matrix)",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/Top%20G%20Caught.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Kai Cenat",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/Kai%20Cenat.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "KSI",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/KSI.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Nick eh 30 NEVER BACK DOWN NEVER GIVE UP",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/Nick%20eh%2030.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "KreekCraft",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/KreekCraft.lua"))();
+   end,
+})
+
+
+-- Meme Section
+local Section = Tab:CreateSection("Memes")
+local Button = Tab:CreateButton({
+   Name = "Kanye meme",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/Kanye%20meme.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Arab Cat",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/Arab%20Cat.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "James Charles (Hi Sisters!)",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/James%20Charles%20hi%20sisters.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Ben",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/Ben.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "The Rock (SUS)",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/The%20Rock%20SUS.lua"))();
+   end,
+})
+
+-- Famous Section
+local Section = Tab:CreateSection("Famous People")
+local Button = Tab:CreateButton({
+   Name = "Ice Spice",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/Ice%20Spice.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Ed Sheeran",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/Ed%20Sheeran.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Travis Scott",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/Travis%20Scott.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Drake",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/Drake.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "DJ Khaled",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/DJ%20Khaled.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Kanye West",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/kanye.lua"))();
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "John Cena",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/bruddaa/starvingartists/raw/main/Art%20Scripts/JohnCena.lua"))();
+   end,
+})
+
+local Tab = Window:CreateTab("Credits")
+local Section = Tab:CreateSection("Join the Discord to suggest more Art")
+local Button = Tab:CreateButton({
+   Name = "Made by Brudda",
+   Callback = function()
+   print("Made by Brudda \nGithub.com/bruddaa/ \nDiscord.gg/3NqYkMFH2C")
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "github.com/bruddaa/",
+   Callback = function()
+   print("Made by Brudda \nGithub.com/bruddaa/ \nDiscord.gg/3NqYkMFH2C")
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "dsc.gg/autodraw",
+   Callback = function()
+   print("Made by Brudda \nGithub.com/bruddaa/ \nDiscord.gg/3NqYkMFH2C")
+   end,
+})
+-- Made by Brudda 
+-- discord.gg/3NqYkMFH2C
+-- github.com/bruddaa/
